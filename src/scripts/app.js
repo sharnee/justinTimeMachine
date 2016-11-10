3052a9b941e538aa0cb2bb7d1570546fef60acc8
Shar'nee Francis <sharneefrancis@Sharnees-MacBook-Pro.local>
2016-11-10 09:48:57 -0600

    added notes to the time machine

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,27 +3,39 @@ import ReactDOM from 'react-dom'
 import STORE from './store'
 
 const app = function() {
+	//First step will be to create the container that will hold the web application
+	//within this web applicaion we will get the initial state of the data to be rendered to the page
+	// we will create an listening event that will allow us to mount the data once an upate is made to it
+	// we will establish when to unmount so that we do not waste space?
+	// we will also render the web app 
 
 	const MachineContainer = React.createClass({
+		//the componentWillMount is a given function that will allow us to mount the desired function to the DOM?
+		
 		componentWillMount: function() {
+			//componentWillMount is a special lifecycle method in react. It runs before render. (keep in mind, this will only run once)
+			//here we have decided that we were going to send out a signal to listen for updates of the data.
 			STORE.on('update', () => {
+				// setState will sync the machineContainer component's state with our STORE's _data property (then re-render)
 				this.setState(STORE._getData())
 			})
 		},
-
+		//turns off event listener
 		componentWillUnmount: function() {
 			STORE.off('update')
 		},
-
+		//Our initial state is established as the initial _data property from STORE
 		getInitialState: function() {
 			return STORE._getData()
 		},
-
+		//renders the page, populating it with the data in our state, which came from STORE
 		render: function() {
 			console.log('machine container',this)
+			//readout is passed the year property from this.state
+			//controlPanel is passed the progress property from this.state
 		 	return (
 		 		<div className='machineContainer' >
-		 			<Readout year={this.state.year} />
+		 			<Readout year={this.state.year} />	
 		 			<ControlPanel progress={this.state.progress} />
 		 		</div>
 		 	)
@@ -32,6 +44,7 @@ const app = function() {
 
 	
 	const Readout = React.createClass({
+		//Readout will finally display the year property in a <p> tag
 		 render: function() {
 		 	return (
 		 		<p className='readout'>
@@ -46,19 +59,17 @@ const app = function() {
 
 
 		_travelBackward: function() {
-			clearInterval(STORE._get('intervalId'))
-			STORE._set({
-				progress: 'backward'
-			})
-			var tickBackward = () => {
+			clearInterval(STORE._get('intervalId')) //stops whatever set interval function is running 
+			var tickBackward = () => { //function to decrement the year by 1 
 				STORE._set({
 					year: STORE._get('year') - 1
 				})
 			}
-			var intervalId = setInterval(tickBackward,500)
-			STORE._set({
-				progress: 'backward',
-				intervalId: intervalId
+			var intervalId = setInterval(tickBackward,500) // runs tickBackward every 500ms AND assings the inervalId the return value of setInterval
+			
+			STORE._set({ 
+				progress: 'backward', // bolds the back button 
+				intervalId: intervalId //
 			})
 
 		},
@@ -66,12 +77,12 @@ const app = function() {
 
 		_travelForward: function() {
 			clearInterval(STORE._get('intervalId'))
-			var tickForward = () => {
+			var tickForward = () => { // increments year by 1 
 				STORE._set({
 					year: STORE._get('year') + 1
 				})
 			}
-			var intervalId = setInterval(tickForward,500)
+			var intervalId = setInterval(tickForward,500) //runs tickForward every 500ms and assigns the return to intervalId
 
 
 			//
@@ -87,7 +98,7 @@ const app = function() {
 			STORE._set('progress','paused')
 		},
 
-		 render: function() {
+		 render: function() { // assigns the fontweight key either a bolder or lighter value depending on the ternery state
 		 	var backwardStyle = {
 		 		'fontWeight': this.props.progress === 'backward' ? 'bolder' : 'lighter' 
 		 	}
@@ -100,9 +111,9 @@ const app = function() {
 
 		 	return (
 		 		<div className="control-panel" >
-	 				<div onClick={this._travelBackward}  style={backwardStyle} className="control-button"><span className="material-icons">{'<<'}</span></div>
-	 				<div onClick={this._stopHere} style={pauseStyle} className="control-button"><span className="material-icons">||</span></div>
-	 				<div onClick={this._travelForward} style={forwardStyle} className="control-button"><span className="material-icons">{'>>'}</span></div>
+	 				<div onClick={this._travelBackward} style={backwardStyle} className="control-button"><span className="material-icons">{'<<'}</span></div>
+	 				<div onClick={this._stopHere} 		style={pauseStyle} 	  className="control-button"><span className="material-icons">||</span></div>
+	 				<div onClick={this._travelForward}  style={forwardStyle}  className="control-button"><span className="material-icons">{'>>'}</span></div>
 		 		</div>
 		 	)
 	 	}
